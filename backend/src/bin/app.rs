2e021dbc59b02562d2b6f038edeae96f9fc7dd1d
masashi99 <sgmdq601@gmail.com>
2025-02-24 13:38:09 +0900

    feat: add logger

--- backend/src/bin/app.rs
+++ backend/src/bin/app.rs
@@ -1,16 +1,47 @@
 use std::net::{Ipv4Addr, SocketAddr};
 
 use adapter::database::connect_database_with;
-use anyhow::Result;
+use anyhow::Context;
 use api::route::{book::build_book_route, health::build_health_check_routes};
 use axum::Router;
 use registry::AppRegistry;
-use shared::config::AppConfig;
+use shared::{
+    config::AppConfig,
+    env::{which, Environment},
+};
 use tokio::net::TcpListener;
+use tower_http::{
+    cors,
+    trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer},
+    LatencyUnit,
+};
+use tracing::Level;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     bootstrap().await
+}
+
+fn init_logger() -> Result<()> {
+    let log_level = match which() {
+        Environment::Development => "debug",
+        Environment::Production => "info",
+    };
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
+
+    let subscriber = tracing_subscriber::fmt::layer()
+        .with_file(true)
+        .with_line_number(true)
+        .with_target(false);
+
+    tracing_subscriber::registry()
+        .with(subscriber)
+        .with(env_filter)
+        .try_init()?;
+
+    Ok(())
 }
 
 async fn bootstrap() -> Result<()> {
@@ -21,11 +52,27 @@ async fn bootstrap() -> Result<()> {
     let app = Router::new()
         .merge(build_health_check_routes())
         .merge(build_book_route())
+        .layer(cors())
+        .layer(
+            TraceLayer::new_for_http()
+                .make_span_with(DefaultMakeSpan::new().level(Level::INFO))
+                .on_request(DefaultOnRequest::new().level(Level::INFO))
+                .on_response(
+                    DefaultOnResponse::new()
+                        .level(Level::INFO)
+                        .latency_unit(LatencyUnit::Millis),
+                ),
+        )
         .with_state(registry);
 
     let addr = SocketAddr::new(Ipv4Addr::LOCALHOST.into(), 8080);
     let listener = TcpListener::bind(addr).await?;
-    println!("Listening on {}", addr);
+    tracing::info!("Listening on {}", addr);
 
-    Ok(axum::serve(listener, app).await?)
+    axum::serve(listener, app)
+        .await
+        .context("Unexpected error happened in server")
+        .inspect_err(
+            |e| tracing::error!(error.cause_chain = ?e,error.message = %e, "Unexpected error"),
+        )
 }
