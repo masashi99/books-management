5f3d527d0b178588ad363b1efd1f34eb73753cb7
masashi99 <sgmdq601@gmail.com>
2025-02-24 14:09:07 +0900

    add: AppResult type

--- backend/adapter/src/repository/book.rs
+++ backend/adapter/src/repository/book.rs
@@ -1,12 +1,11 @@
-use anyhow::Result;
 use async_trait::async_trait;
 use derive_new::new;
 use kernel::{
     model::book::{event::CreateBook, Book},
     repository::book::BookRepository,
 };
-use sqlx;
-use uuid::Uuid;
+use shared::error::{AppError, AppResult};
+use sqlx::{self, types::Uuid};
 
 use crate::database::model::book::BookRow;
 use crate::database::ConnectionPool;
@@ -18,7 +17,7 @@ pub struct BookRepositoryImpl {
 
 #[async_trait]
 impl BookRepository for BookRepositoryImpl {
-    async fn create(&self, event: CreateBook) -> Result<()> {
+    async fn create(&self, event: CreateBook) -> AppResult<()> {
         sqlx::query!(
             r#"
                 INSERT INTO books (title, author, isbn, description)
@@ -30,11 +29,12 @@ impl BookRepository for BookRepositoryImpl {
             event.description
         )
         .execute(self.db.inner_ref())
-        .await?;
+        .await
+        .map_err(AppError::SpecificOperationError)?;
 
         Ok(())
     }
-    async fn find_all(&self) -> Result<Vec<Book>> {
+    async fn find_all(&self) -> AppResult<Vec<Book>> {
         let rows = sqlx::query_as!(
             BookRow,
             r#"
@@ -49,11 +49,12 @@ impl BookRepository for BookRepositoryImpl {
             "#
         )
         .fetch_all(self.db.inner_ref())
-        .await?;
+        .await
+        .map_err(AppError::SpecificOperationError)?;
 
         Ok(rows.into_iter().map(Book::from).collect())
     }
-    async fn find_by_id(&self, book_id: Uuid) -> Result<Option<Book>> {
+    async fn find_by_id(&self, book_id: Uuid) -> AppResult<Option<Book>> {
         let row: Option<BookRow> = sqlx::query_as!(
             BookRow,
             r#"
@@ -69,7 +70,8 @@ impl BookRepository for BookRepositoryImpl {
             book_id
         )
         .fetch_optional(self.db.inner_ref())
-        .await?;
+        .await
+        .map_err(AppError::SpecificOperationError)?;
 
         Ok(row.map(Book::from))
     }
